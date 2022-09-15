1c3577224991d3cb7578682bc43826d7e87e2b16
brian corker <bcorker123@gmail.com>
2022-09-15 16:08:43 -0400

    added app and user controller, added backend routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get '/me', to: "users#show"
+  post '/signup', to: "users#create"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
   scope '/api' do 
     resources :comments
     resources :ratings
